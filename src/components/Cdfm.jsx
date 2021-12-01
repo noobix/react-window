3369522a515ae0ceb640e5ac3083ff4bfb7787f1
Kelvin Kabute <skipgh@gmail.com>
2021-12-01 15:31:13 +0000

    initial commit

---
+++ src/components/Cdfm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+let Freefox =  () => {
+    return(
+        <div>
+            <h1>
+            “Codetrain is awesome, react works!!!!” 
+            </h1>
+        </div>
+    );
+}
+
+export default Freefox
